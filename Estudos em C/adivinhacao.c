82ab6516adad56b47280ca93e2c0f5236c61409d
rodrigormachado <rodrigoribeiromachado2@gmail.com>
2016-08-01 21:36:17 -0300

    Continuando estudos, agora na linguagem C

---
+++ Estudos em C/adivinhacao.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+printf("*************************************\n");
+printf("* Bem vindo ao jogo de adivinhacao! *\n");
+printf("*************************************\n");
+}
